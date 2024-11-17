01d7a259dc0e4f5738db9cb47032769e09fb591c
sashamarfuttech <sasha.marfut.tech@gmail.com>
2024-11-17 15:34:20 +0100

    app service + database integration

--- Lesson4/Web Service Integrated with Database/Program.cs
+++ Lesson4/Web Service Integrated with Database/Program.cs
@@ -1,3 +1,8 @@
+
+using System.Data.SqlClient;
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,13 +21,46 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/text", () =>
+app.MapGet("/database", () =>
 {
-    //TODO: database integration
+        var connectionString = "Server=tcp:omarfut-server.database.windows.net,1433;Initial Catalog=test;Persist Security Info=False;User ID=omarfut;Password=81Osuzov;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        
+        using var connection = new SqlConnection(connectionString);
+        
+        connection.Open();
 
-    return "some text";
+        using var command = new SqlCommand("SELECT [Name] FROM [Users]", connection);
+        using var reader = command.ExecuteReader();
+
+        Object data = 0;
+
+        while (reader.Read())
+        {
+            data = reader.GetString(0);
+        }
+
+    return data;
 })
-.WithName("GetText")
+.WithName("Database")
+.WithOpenApi();
+
+app.MapGet("/blob", async () =>
+{
+    const string connectionString = "DefaultEndpointsProtocol=https;AccountName=testomarfut123;AccountKey=AgIjuQSMaw6iq9gKDP0hJIGe0ephPoHnjil9Q/1JQXGJhIbROGPZiI/cz6WPtLBUXZsCglJSBxB++AStQMi1yA==;EndpointSuffix=core.windows.net";
+    const string containerName = "cont";
+    const string blobName = "data.txt";
+
+    var blobServiceClient = new BlobServiceClient(connectionString);
+    var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+    var blobClient = containerClient.GetBlobClient(blobName);
+
+    var localFilePath = Path.Combine(Path.GetTempPath(), blobName);
+    await blobClient.DownloadToAsync(localFilePath);
+
+    string[] lines = File.ReadAllLines(localFilePath);
+    return lines;
+})
+.WithName("Blob")
 .WithOpenApi();
 
 app.Run();
